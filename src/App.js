47a5bf812a17b3379f6e2706b7d0b5a8845b6506
Ryan Weekley <rweekley@cyberalsolutions.com>
2021-11-12 07:45:57 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import Todo from "./components/Todo";
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storagetodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (storagetodos) {
+      setTodos(storagetodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React ToDo</p>
+        <TodoForm addTodo={addTodo}></TodoForm>
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
